ef19ebc521d9ed8e609c818ec9a925352c736ae1
krb3p <ryankatherineburke@gmail.com>
2017-07-15 18:41:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0
+  function getBumps() {
+    return counter
+  }
+  function addBump() {
+    return counter++
+  }
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice }
+  }
+}
+
+let sharkCreator = createAnimal('Shark')
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+let sharkWithFrickinCannon = sharkCreator('Cannon')
